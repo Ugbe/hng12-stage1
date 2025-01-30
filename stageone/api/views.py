5adcbffdf4bcb1c29e08306fa0ae062ce8a6f62e
Ugbe <ugbedeojoabba@gmail.com>
2025-01-30 05:04:59 +0100

    fixed datetime

--- stageone/api/views.py
+++ stageone/api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from datetime import datetime, timezone
-import requests
+
 
 def service(request):
     response_data = {
         "email": "ugbedeojoabba@gmail.com",
-        "current_datetime": datetime.now(timezone.utc).isoformat(timespec='seconds'),
+        "current_datetime": datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
         "github_url": "https://github.com/Ugbe/hng12-stage1"
     }
     return JsonResponse(response_data, safe=False)
